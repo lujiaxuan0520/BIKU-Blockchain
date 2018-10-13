e26fda9d6071c1417f31ede9466d644de9d4cebd
鲍佳麒 <249961086@qq.com>
2018-10-14 00:07:03 +0800

    push test

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ from app import app
 import sys
 import os
 from flask_script import Manager
+
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # sys.path.insert(0, os.path.join(BASE_DIR, 'movie_project\\app'))
 # sys.path.insert(0, os.path.join(BASE_DIR, 'movie_project\\app\\admin'))
